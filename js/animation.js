83291dde6c7662ef1cdf04104d6856dbea500c3c
Gordey <112198400+Finyi@users.noreply.github.com>
2023-11-21 18:05:17 +0200

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,27 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.7] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let mapmobile = document.querySelectorAll('.img__alphamapmobile');
+  for (let elm of mapmobile) {
+    observer.observe(elm);
+  }
+
+  let mapdesktop = document.querySelectorAll('.img__alphamapdesktop');
+  for (let elm of mapdesktop) {
+    observer.observe(elm)
+  }
+
+  let blueflag = document.querySelectorAll('.content__blueflag');
+  for (let elm of blueflag) {
+    observer.observe(elm)
+  }
+  let yellowflag = document.querySelectorAll('.content__yellowflag');
+  for (let elm of yellowflag) {
+    observer.observe(elm)
+  }
