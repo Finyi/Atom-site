a43fc845c8349664297a12287fc4fe1e2ee2a4a9
Gordey <112198400+Finyi@users.noreply.github.com>
2023-12-02 21:03:54 +0200

    corrections

--- js/textUP.js
+++ js/textUP.js
@@ -7,5 +7,7 @@ let showText = function(target, message, index, interval) {
       }, interval);
     }
   }
-  showText("#alphamap__text", "AlphaMap", 0, 300)
-  showText("#main_project_text", "Projects /", 0, 80)
+  showText("#alphamap__text", "AlphaMap", 0, 100)
+  showText("#alphamapsite__text", "AlphaMap Site", 0, 100)
+  showText("#card__alphamap__text", "A Leafletjs based project with plugins from Leafletjs users. A project showing the war in Ukraine on a map.", 0, 100)
+  showText("#card__alphamapsite__text", "Future website-blog about the terrible war on AlphaMap in Ukraine.", 0, 100)
